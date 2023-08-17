1fe4affffd4791c8b61808a7fd57a280945be3ed
sarfish <2548616851@qq.com>
2023-08-17 16:28:35 +0800

    Lab2 Ex5

--- kern/pmap.c
+++ kern/pmap.c
@@ -175,7 +175,10 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-
+	boot_map_region(kern_pgdir, 
+		UPAGES, npages*sizeof(struct PageInfo), 
+		PADDR(pages),
+		PTE_U);
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
@@ -187,7 +190,10 @@ mem_init(void)
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
-
+	boot_map_region(kern_pgdir,
+		KSTACKTOP - KSTKSIZE, KSTKSIZE,
+		PADDR(bootstack),
+		PTE_W);
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
 	// Ie.  the VA range [KERNBASE, 2^32) should map to
@@ -196,7 +202,11 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
-
+	boot_map_region(kern_pgdir,
+		KERNBASE,
+		0xFFFFFFFF - KERNBASE,
+		0,
+		PTE_W);
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
 
